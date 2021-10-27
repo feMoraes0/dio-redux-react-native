47b5d0b1fca03aa7b13a6f088dfdf769558980cd
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-10-27 19:07:40 -0300

    feat(status-bar): adds status bar with background dark and style content light

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import Routes from './src/routes';
 
-const App = () => <Routes />;
+const App = () => (
+  <>
+    <Routes />
+    <StatusBar backgroundColor="#312e38" barStyle="light-content" />
+  </>
+);
 
 export default App;
