a5e2f8946aeec8b149323cf6161654de0e29c66a
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-11-02 22:00:00 -0300

    feat(cart): adds new cart ui with a list of products and total at the footer

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,109 @@
-import React from 'react';
-import { Container } from './styles';
+import { Feather } from '@expo/vector-icons';
+/**
+ * useMemo atualiza o component apenas quando a variavel de dependencia dele muda
+ */
+import React, { useState, useMemo } from 'react';
+import { View } from 'react-native';
+import formatValue from '../../utils/formatValue';
+import {
+  ActionButton,
+  ActionContainer,
+  Container,
+  Product,
+  ProductContainer,
+  ProductImage,
+  ProductList,
+  ProductPrice,
+  ProductPriceContainer,
+  ProductQuantity,
+  ProductSinglePrice,
+  ProductTitle,
+  ProductTitleContainer,
+  SubTotalValue,
+  TotalContainer,
+  TotallProductsText,
+  TotalProductsContainer,
+} from './styles';
 
-const Cart = () => <Container />;
+const Cart = () => {
+  const [products] = useState([
+    {
+      id: '1',
+      title: 'Assinatura Trimestral',
+      image_url:
+        'https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png',
+      price: 150,
+      quantity: 1,
+    },
+    {
+      id: '2',
+      title: 'Assinatura Anual',
+      image_url:
+        'https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/annual_subscription_qyolci.png',
+      price: 540,
+      quantity: 2,
+    },
+  ]);
+
+  const cartSize = useMemo(() => products.length || 0, [products]);
+
+  const cartTotal = useMemo(() => {
+    const cartAmount = products.reduce((accumulator, product) => {
+      const totalPrice = accumulator + product.price * product.quantity;
+      return totalPrice;
+    }, 0);
+    return formatValue(cartAmount);
+  }, [products]);
+
+  return (
+    <Container>
+      <ProductContainer>
+        <ProductList
+          data={products}
+          keyExtractor={(item) => item.id}
+          ListFooterComponent={<View />}
+          ListFooterComponentStyle={{
+            height: 80.0,
+          }}
+          renderItem={({ item }) => (
+            <Product>
+              <ProductImage source={{ uri: item.image_url }} />
+              <ProductTitleContainer>
+                <ProductTitle>{item.title}</ProductTitle>
+                <ProductPriceContainer>
+                  <ProductSinglePrice>
+                    {formatValue(item.price)}
+                  </ProductSinglePrice>
+                  <TotalContainer>
+                    <ProductQuantity>{`${item.quantity}x`}</ProductQuantity>
+                    <ProductPrice>
+                      {formatValue(item.price * item.quantity)}
+                    </ProductPrice>
+                  </TotalContainer>
+                </ProductPriceContainer>
+              </ProductTitleContainer>
+              <ActionContainer>
+                <ActionButton onPress={() => {}}>
+                  <Feather name="plus" color="#e83f5b" size={16} />
+                </ActionButton>
+                <ActionButton onPress={() => {}}>
+                  <Feather name="minus" color="#e83f5b" size={16} />
+                </ActionButton>
+              </ActionContainer>
+            </Product>
+          )}
+        />
+      </ProductContainer>
+      <TotalProductsContainer>
+        <Feather name="shopping-cart" color="#fff" size={24} />
+        <TotallProductsText>
+          {cartSize} {cartSize === 1 ? 'item' : 'itens'}
+        </TotallProductsText>
+        <SubTotalValue>{cartTotal}</SubTotalValue>
+      </TotalProductsContainer>
+    </Container>
+  );
+};
 
 export default {
   name: 'Cart',
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,7 +1,115 @@
 import styled from 'styled-components';
+import { FlatList } from 'react-native';
 
-export const Container = styled.View`
-  flex: 1;
-  padding: 30px;
+export const Container = styled.SafeAreaView`
   background-color: #333;
+  flex: 1;
+`;
+
+export const ProductContainer = styled.View`
+  background-color: #e83f5b;
+  border-radius: 20px;
+  flex-direction: row;
+  flex: 1;
+  margin-top: 60px;
+`;
+
+export const ProductList = styled(FlatList)`
+  flex: 1;
+  padding: 30px 10px;
+`;
+
+export const Product = styled.View`
+  background-color: #fff;
+  border-radius: 20px;
+  flex-direction: row;
+  flex: 1;
+  margin: 5px;
+  min-height: 130px;
+  min-width: 350px;
+  padding: 15px 10px;
+`;
+
+export const ProductImage = styled.Image`
+  height: 92px;
+  width: 140px;
+`;
+
+export const ProductTitleContainer = styled.View`
+  font-size: 16px;
+  margin-left: 5px;
+  margin-top: 10px;
+`;
+
+export const ProductTitle = styled.Text`
+  font-size: 16px;
+`;
+
+export const ProductPriceContainer = styled.View`
+  flex-direction: column;
+`;
+
+export const TotalContainer = styled.View`
+  flex-direction: row;
+  margin-top: 8px;
+`;
+
+export const ProductSinglePrice = styled.Text`
+  color: #414756;
+  font-size: 12px;
+  margin-top: 8px;
+`;
+
+export const ProductPrice = styled.Text`
+  color: #e83f5b;
+  font-weight: bold;
+  font-size: 16px;
+  margin-top: 5px;
+`;
+
+export const ProductQuantity = styled.Text`
+  color: #e83f5b;
+  font-size: 16px;
+  font-weight: bold;
+  margin-right: 10px;
+  margin-top: 5px;
+`;
+
+export const ActionContainer = styled.View`
+  align-self: flex-end;
+  align-items: center;
+  justify-content: space-between;
+  margin-left: auto;
+`;
+
+export const ActionButton = styled.TouchableOpacity`
+  background-color: rgba(232, 63, 91, 0.1);
+  border-radius: 5px;
+  padding: 12px;
+  margin-bottom: 5px;
+`;
+
+export const TotalProductsContainer = styled.View`
+  align-items: center;
+  background-color: #e83f5b;
+  border-radius: 10px;
+  bottom: 0;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 36px 40px;
+  position: absolute;
+`;
+
+export const TotallProductsText = styled.Text`
+  color: #fff;
+  flex: 1;
+  font-size: 16px;
+  font-weight: bold;
+  margin-left: 15px;
+`;
+
+export const SubTotalValue = styled.Text`
+  font-size: 16px;
+  color: #fff;
+  font-weight: bold;
 `;
